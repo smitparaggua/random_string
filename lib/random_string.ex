6db249dbe2e75d8041eb86823d7c01e61c938576
John Smith Paraggua <smitparaggua@gmail.com>
2018-07-10 22:24:59 +0800

    Added way to limit length

--- lib/random_string.ex
+++ lib/random_string.ex
@@ -1,18 +1,24 @@
 defmodule RandomString do
-  @moduledoc """
-  Documentation for RandomString.
-  """
+  @numeric ~w(1 2 3 4 6 7 8 9 0)
+  @alpha ~w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+  @alphanumeric @alpha ++ @numeric
 
-  @doc """
-  Hello world.
+  @default_length 32
 
-  ## Examples
+  def generate do
+    generate(@default_length)
+  end
 
-      iex> RandomString.hello
-      :world
+  def generate(length) when is_integer(length) do
+    characters = @alphanumeric
+    character_count = Enum.count(characters)
+    Stream.repeatedly(fn -> random_character(characters, character_count) end)
+    |> Stream.take(length)
+    |> Enum.join("")
+  end
 
-  """
-  def hello do
-    :world
+  defp random_character(characters, character_count) do
+    random_index = :rand.uniform(character_count) - 1
+    Enum.at(characters, random_index)
   end
 end
--- test/random_string_test.exs
+++ test/random_string_test.exs
@@ -2,7 +2,25 @@ defmodule RandomStringTest do
   use ExUnit.Case
   doctest RandomString
 
-  test "greets the world" do
-    assert RandomString.hello() == :world
+  test "by default returns a string 32 characters long" do
+    assert String.length(RandomString.generate()) == 32
+  end
+
+  test "by default returns alpha-numeric" do
+    assert String.match?(RandomString.generate(), ~r/^[0-9a-z]*$/i)
+  end
+
+  test "changes every call" do
+    first = RandomString.generate()
+    second = RandomString.generate()
+    third = RandomString.generate()
+    refute first == second
+    refute first == third
+    refute second == third
+  end
+
+  test "when an integer is passed, return string of that length" do
+    string = RandomString.generate(21)
+    assert String.length(string) == 21
   end
 end
